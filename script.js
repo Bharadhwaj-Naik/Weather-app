9fad5b52e99c93b7bfce7df579d2a498ccc0eb68
Bharadhwaj-Naik <bharadhwaj.naik@gmail.com>
2026-07-12 19:15:09 +0530

    linked js file and defined DOM  elements

---
+++ script.js
@@ -0,0 +1,25 @@
+(function() {
+      // ---------- API configuration ----------
+      const API_KEY = 'bd5e378503939ddaee76f12ad7a97608'; // OpenWeatherMap free API key
+      const BASE_URL = 'https://api.openweathermap.org/data/2.5';
+      
+      // DOM elements
+      const cityInput = document.getElementById('cityInput');
+      const searchBtn = document.getElementById('searchBtn');
+      const historyTagsDiv = document.getElementById('historyTags');
+      const clearHistoryBtn = document.getElementById('clearHistoryBtn');
+      const currentCard = document.getElementById('currentCard');
+      const cityDisplay = document.getElementById('cityDisplay');
+      const countryBadge = document.getElementById('countryBadge');
+      const currentDate = document.getElementById('currentDate');
+      const humiditySpan = document.getElementById('humidity');
+      const windSpan = document.getElementById('wind');
+      const currentWeatherIcon = document.getElementById('currentWeatherIcon');
+      const currentTemp = document.getElementById('currentTemp');
+      const descriptionSpan = document.getElementById('description');
+      const forecastGrid = document.getElementById('forecastGrid');
+      const errorBox = document.getElementById('errorBox');
+
+      
+    }
+  ) ();
